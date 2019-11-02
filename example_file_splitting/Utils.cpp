dc76d7b746a468b30c3fc9aad2ee04f1938ff276
antoinepetty <36314396+antoinepetty@users.noreply.github.com>
2019-11-02 11:37:01 +0000

    Example to show how c++ headers work

---
+++ example_file_splitting/Utils.cpp
@@ -0,0 +1,11 @@
+#include "Utils.h"
+
+int numberVar = 5;
+
+int getVariable(){
+  return numberVar;
+}
+
+void setVariable(int var){
+  numberVar = var;
+}
---
+++ example_file_splitting/example_file_splitting.ino
@@ -0,0 +1,11 @@
+#include "Utils.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  int testnum = getVariable() * 2;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
